eda6abe360bad4ff14991cc77921960271a7326d
tuttlen2027 <ntuttle.reporter@sbcglobal.net>
2026-06-08 14:29:24 -0400

    Add files via upload

---
+++ project3/js/footer_navigation.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+/* This is an example of footer navigational text hyperlinks */
+/* Use the script tag to reference this external file from within your HTML document in the head section */
+
+document.write("<div style='text-align:center;'>");
+document.write("<a href='index.htm'>Welcome</a> | <a href='rules.html'>Rules</a> | <a href='submit.html'>Submit</a> | <a href='categories.html'>Categories</a> | <a href='voting.html'>Voting</a> | <a href='gallery.html'>Gallery</a>");
+document.write("</div>");
